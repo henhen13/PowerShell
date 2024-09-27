0b62d8b33c1bf053038e940f3c3a9ee462e08420
henhen13 <152538123+henhen13@users.noreply.github.com>
2024-09-27 12:59:34 +0200

    Add files via upload

---
+++ dhcpsave.ps1
@@ -0,0 +1,13 @@
+﻿# Define the output file path
+$outputFilePath = "C:\Windows\Temp\DHCPLeases.csv"
+
+# Retrieve DHCP leases
+$dhcpLeases = Get-DhcpServerv4Lease -ScopeId 172.16.11.0 -ComputerName HENHEN-DC01
+
+# Select required properties and create custom objects
+$leaseData = $dhcpLeases | Select-Object ScopeId, IPAddress, HostName, ClientID, AddressState
+
+# Export to CSV with semicolon delimiter
+$leaseData | Export-Csv -Path $outputFilePath -Delimiter ';' -NoTypeInformation -Encoding UTF8
+
+Write-Host "DHCP leases have been exported to $outputFilePath"
